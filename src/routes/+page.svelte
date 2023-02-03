7394fe62ed6aa034b318b626b2fdfd4854666394
Dusan Turajlic <dusan@turajlci.fi>
2023-02-03 20:16:52 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="bg-white">
+  <div class="mx-auto max-w-7xl py-16 px-6 sm:py-24 lg:px-8">
+    <div class="text-center">
+      <h1 class="text-lg font-semibold text-indigo-600">Welcome to SvelteKit</h1>
+      <p>Visit <a class="text-base font-medium text-indigo-700" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the  33 documentation</p>
+    </div>
+  </div>
+</div>
